345f7524acb0a8812545cba9860bc9aa96242285
Andrest2199 <128836304+Andrest2199@users.noreply.github.com>
2024-10-03 11:52:22 -0600

    Modify connection to mongoose.

--- utils/db.js
+++ utils/db.js
@@ -4,10 +4,17 @@ const dotenv = require('dotenv');
 dotenv.config();
 
 const connectDB = async () => {
-  if (mongoose.connection.readyState === 0) {
+  if (mongoose.connection.readyState === 1) {
+    console.log('Conexión existente a MongoDB');
+    return;
+  }
+  
+  try {
     await mongoose.connect(process.env.MONGODB_URI);
     console.log('Conectado a MongoDB');
+  } catch (error) {
+    console.error('Error al conectar a MongoDB:', error);
+    throw new Error('Error al conectar a MongoDB');
   }
 };
-
 module.exports = connectDB;
